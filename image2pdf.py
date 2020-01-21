6394c2a6f8c111b2cde3e820cd4726e98e05ea27
AmirSina Mandegari <mandegari.amirsina@gmail.com>
2020-01-21 20:26:50 +0330

    initial commit to add script file

---
+++ image2pdf.py
@@ -0,0 +1,36 @@
+import os
+import img2pdf
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--input_path",
+    type=str,
+    default=".",
+    help="input folder path"
+    )
+parser.add_argument(
+    "-output_path",
+    type=str,
+    default=".",
+    help="output file path"
+    )
+parser.add_argument(
+    "--output_name",
+    type=str,
+    default="output.pdf",
+    help="output file name"
+    )
+
+args = parser.parse_args()
+files = os.listdir(os.path.abspath(args.input_path))
+files.sort()
+
+output_name = args.output_name
+if not output_name.endswith(".pdf"):
+    output_name = output_name + ".pdf"
+
+output_file = os.path.join(os.path.abspath(args.output_path), output_name)
+with open(output_file, "wb") as f:
+    f.write(img2pdf.convert([i for i in files if i.endswith(".jpg")]))
+print('done')
